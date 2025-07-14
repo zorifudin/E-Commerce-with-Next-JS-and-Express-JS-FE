5a1524ad2741f65aac54fc2fd8b96a61d0fc6bb4
Abdurrohman Zorif <zorifimaduddin@gmail.com>
2025-07-14 08:03:35 +0700

    feat:add use register (#1)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import ReactQueryProvider from "@/providers/ReactQueryProvider";
+import { ToastContainer, toast } from "react-toastify";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
-        {children}
+        <ReactQueryProvider>
+          <Navbar />
+          {children}
+        </ReactQueryProvider>
+        <ToastContainer />
       </body>
     </html>
   );
--- src/hooks/api/auth/useRegister.ts
+++ src/hooks/api/auth/useRegister.ts
@@ -1,7 +1,31 @@
 "use client";
 
+import { axiosInstance } from "@/lib/axios";
+import { useMutation } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
+import { toast } from "react-toastify";
+
+interface RegisterPayload {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  password: string;
+}
+
 const useRegister = () => {
-  return {};
+  return useMutation({
+    mutationFn: async (payload: RegisterPayload) => {
+      const { data } = await axiosInstance.post("auth/register", payload);
+      return data;
+    },
+    onSuccess: () => {
+      toast.success("Register success");
+    },
+    onError: (error: AxiosError<any>) => {
+      toast.error(error.response?.data);
+    },
+  });
 };
 
 export default useRegister;
