33431a362683a1f09c847e32c657a06ef3b6f2bc
zorifudin <zorifimaduddin@gmail.com>
2025-07-14 19:29:31 +0700

    feat:login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import LoginPage from "@/features/login";
+
+const Login = () => {
+  return (
+    <div>
+      <LoginPage />
+    </div>
+  );
+};
+
+export default Login;
--- src/features/login/index.tsx
+++ src/features/login/index.tsx
@@ -1,20 +1,19 @@
 "use client";
 
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Separator } from "@/components/ui/separator";
-import Link from "next/link";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { useState } from "react";
+import { Separator } from "@/components/ui/separator";
 import { useFormik } from "formik";
-import { Lasso } from "lucide-react";
-import useRegister from "@/hooks/api/auth/useRegister";
-import { RegisterSchema } from "./schemas";
+import Link from "next/link";
+import React, { useState } from "react";
+import { LoginSchema } from "./schemas";
+import useLogin from "@/hooks/api/auth/useLogin";
 
-const RegisterPage = () => {
-  const { mutateAsync: register, isPending } = useRegister();
+const LoginPage = () => {
+  const { mutateAsync: login, isPending } = useLogin();
 
   const formik = useFormik({
     initialValues: {
@@ -24,20 +23,19 @@ const RegisterPage = () => {
       phoneNumber: "",
       password: "",
     },
-    validationSchema: RegisterSchema,
+    validationSchema: LoginSchema,
     onSubmit: async (values) => {
-      await register(values);
+      await login(values);
     },
   });
 
   const [isChecked, setIsChecked] = useState(false);
-
   return (
     <main className="container mx-auto flex min-h-[80vh] flex-col items-center justify-center px-4 py-8">
       <Card className="w-full max-w-md shadow-lg">
         <CardHeader className="pb-6 text-center">
           <CardTitle className="text-2xl font-bold text-green-500 sm:text-3xl">
-            Register Now
+            Welcome Back!
           </CardTitle>
         </CardHeader>
 
@@ -59,61 +57,6 @@ const RegisterPage = () => {
             className="space-y-3 sm:space-y-4"
             onSubmit={formik.handleSubmit}
           >
-            <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 sm:gap-4">
-              <div>
-                <Input
-                  type="text"
-                  name="firstName"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.firstName}
-                  autoComplete="off"
-                  placeholder="First name"
-                  className="rounded-md border border-gray-300 px-3 py-2 text-sm focus:border-transparent focus:ring-2 focus:ring-green-500 focus:outline-none sm:px-4 sm:py-3 sm:text-base"
-                />
-                {formik.touched.firstName && formik.errors.firstName ? (
-                  <div className="text-muted-foreground text-sm">
-                    {formik.errors.firstName}
-                  </div>
-                ) : null}
-              </div>
-              <div>
-                <Input
-                  type="text"
-                  name="lastName"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.lastName}
-                  autoComplete="off"
-                  placeholder="Last Name"
-                  className="rounded-md border border-gray-300 px-3 py-2 text-sm focus:border-transparent focus:ring-2 focus:ring-green-500 focus:outline-none sm:px-4 sm:py-3 sm:text-base"
-                />
-                {formik.touched.lastName && formik.errors.lastName ? (
-                  <div className="text-muted-foreground text-sm">
-                    {formik.errors.lastName}
-                  </div>
-                ) : null}
-              </div>
-            </div>
-
-            <div>
-              <Input
-                type="tel"
-                name="phoneNumber"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.phoneNumber}
-                autoComplete="off"
-                placeholder="Phone Number"
-                className="rounded-md border border-gray-300 px-3 py-2 text-sm focus:border-transparent focus:ring-2 focus:ring-green-500 focus:outline-none sm:px-4 sm:py-3 sm:text-base"
-              />
-              {formik.touched.phoneNumber && formik.errors.phoneNumber ? (
-                <div className="text-muted-foreground text-sm">
-                  {formik.errors.phoneNumber}
-                </div>
-              ) : null}
-            </div>
-
             <div>
               <Input
                 type="text"
@@ -165,18 +108,18 @@ const RegisterPage = () => {
               type="submit"
               className="w-full rounded-md bg-green-500 py-2 text-sm font-medium text-white hover:bg-green-600 sm:py-3 sm:text-base"
             >
-              {isPending ? "Loading..." : "Sign Up"}
+              {isPending ? "Loading..." : "Log In"}
             </Button>
           </form>
 
           <div className="text-center">
             <p className="text-sm text-gray-600">
-              Already have an account?{" "}
+              Don't have an account?{" "}
               <Link
-                href="/login"
+                href="/register"
                 className="text-green-500 hover:text-green-600"
               >
-                Log In.
+                Sign Up.
               </Link>
             </p>
           </div>
@@ -186,4 +129,4 @@ const RegisterPage = () => {
   );
 };
 
-export default RegisterPage;
+export default LoginPage;
